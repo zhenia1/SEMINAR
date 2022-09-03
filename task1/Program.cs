232d001764c723a0e788d64069bce9b51f9cf9c3
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-04 01:38:44 +0500

    add SEMINAR

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+Console.WriteLine(numberS.Length == 3);
+
---
+++ task1/task2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+
+Console.WriteLine(numberS.Length > 2);
